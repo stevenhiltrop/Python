8908d02a0a8c874e5643eb8514514e4a07a2d5da
Steven Hiltrop <24697316+stevenhiltrop@users.noreply.github.com>
2020-01-01 10:13:34 +1100

    Add files via upload

---
+++ Exercism/python/matrix/matrix.py
@@ -0,0 +1,9 @@
+class Matrix:
+    def __init__(self, matrix_string):
+        self.rows = [map(int, r.split()) for r in matrix_string.splitlines()]
+
+    def row(self, index):
+        return self.rows[index - 1]
+
+    def column(self, index):
+        return [row[index - 1] for row in self.rows]
