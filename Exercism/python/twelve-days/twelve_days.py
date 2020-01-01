dac9b58ebd71c2e8a1e5044e7ad14e29a96e1d62
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-01-01 12:09:12 +1100

    Added: twelve-days

---
+++ Exercism/python/twelve-days/twelve_days.py
@@ -0,0 +1,41 @@
+def recite(start_verse, end_verse):
+    days = {
+        1: "first",
+        2: "second",
+        3: "third",
+        4: "fourth",
+        5: "fifth",
+        6: "sixth",
+        7: "seventh",
+        8: "eighth",
+        9: "ninth",
+        10: "tenth",
+        11: "eleventh",
+        12: "twelfth"
+    }
+
+    christmas_presents = [
+        'a Partridge in a Pear Tree.',
+        'two Turtle Doves',
+        'three French Hens',
+        'four Calling Birds',
+        'five Gold Rings',
+        'six Geese-a-Laying',
+        'seven Swans-a-Swimming',
+        'eight Maids-a-Milking',
+        'nine Ladies Dancing',
+        'ten Lords-a-Leaping',
+        'eleven Pipers Piping',
+        'twelve Drummers Drumming'
+    ]
+
+    """
+    On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.
+    """
+    if end_verse > 1:
+        christmas_presents[0].replace(christmas_presents[0], 'and '.join(christmas_presents[0]))
+        presents = ', '.join(present for present in christmas_presents)
+    else:
+        presents = christmas_presents[0]
+
+    return "On the {0} day of Christmas my true love gave to me: {1}".format(days[start_verse], presents)
