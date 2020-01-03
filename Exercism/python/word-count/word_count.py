6509e83a1b537321303ccf01b2d381899838c1af
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-01-03 18:35:23 +1100

    Added: word_count apostrophe exception

--- Exercism/python/word-count/word_count.py
+++ Exercism/python/word-count/word_count.py
@@ -6,10 +6,12 @@ def count_words(sentence):
     word_dict = dict()
 
     for word in word_list:
-        if not word.isalnum() and word:  # Remove special and empty characters
-            if word is "'":
-                pass
-            else:
+        if "'" in word \
+                and not word.startswith("'") \
+                and not word.endswith("'"):  # Keep apostrophe in words but not words that are quoted
+            pass
+        else:
+            if not word.isalnum() and word:  # Remove special and empty characters
                 word = ''.join(character for character in word if character.isalnum())
 
         if word in word_dict:
@@ -17,7 +19,7 @@ def count_words(sentence):
         else:
             word_dict[word] = 1
 
-    if '' in word_dict:
+    if '' in word_dict:  # Remove nullbyte from dictionary
         word_dict.pop('')
 
     return word_dict
