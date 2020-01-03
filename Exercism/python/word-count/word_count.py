d5c43c4c373213e082eea6c463220c1e8e9829fd
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-01-03 18:26:32 +1100

    Added: word_count functionality

--- Exercism/python/word-count/word_count.py
+++ Exercism/python/word-count/word_count.py
@@ -1,2 +1,23 @@
+import re
+
+
 def count_words(sentence):
-    pass
+    word_list = re.split(",| |\"|\n|\t|\_|\.", sentence.lower())
+    word_dict = dict()
+
+    for word in word_list:
+        if not word.isalnum() and word:  # Remove special and empty characters
+            if word is "'":
+                pass
+            else:
+                word = ''.join(character for character in word if character.isalnum())
+
+        if word in word_dict:
+            word_dict[word] += 1
+        else:
+            word_dict[word] = 1
+
+    if '' in word_dict:
+        word_dict.pop('')
+
+    return word_dict
