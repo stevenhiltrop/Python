6d0dc27f58526c1fdc1a61b48e0eb24009b8a806
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-01-19 22:55:35 +0700

    Changed: acronym.py

--- Exercism/python/acronym/acronym.py
+++ Exercism/python/acronym/acronym.py
@@ -2,11 +2,9 @@ import re
 
 
 def abbreviate(words):
-    words = re.findall(r"[\w']+", words)
     acronym = str()
 
-    for word in words:
-        word = ''.join(e for e in word if e.isalnum())
+    for word in re.findall(r"[\w']+", words):
         acronym += ''.join(word[:1].capitalize())
 
     return acronym.upper()
