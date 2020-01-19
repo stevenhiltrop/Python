ea436e4765abfc95e53cd6bffc8d3c29aeb0b103
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-01-20 00:38:01 +0700

    Changed: kindergarten_garden.py final touches

--- Exercism/python/kindergarten-garden/kindergarten_garden.py
+++ Exercism/python/kindergarten-garden/kindergarten_garden.py
@@ -1,22 +1,19 @@
 class Garden:
-    def __init__(self, diagram, students=list()):
-        self.plant = {
-            "G": "Grass",
-            "C": "Clover",
-            "R": "Radishes",
-            "V": "Violets"
-        }
+    PLANTS = {
+        "G": "Grass",
+        "C": "Clover",
+        "R": "Radishes",
+        "V": "Violets"
+    }
+    STUDENTS = [
+        "Alice", "Bob", "Charlie", "David",
+        "Eve", "Fred", "Ginny", "Harriet",
+        "Ileana", "Joseph", "Kincaid", "Larry"
+    ]
 
-        if diagram:
-            self.diagram = diagram.rsplit()
-        if students:
-            self.students = sorted(students)
-        else:
-            self.students = [
-                "Alice", "Bob", "Charlie", "David",
-                "Eve", "Fred", "Ginny", "Harriet",
-                "Ileana", "Joseph", "Kincaid", "Larry"
-            ]
+    def __init__(self, diagram, students=STUDENTS):
+        self.diagram = diagram.rsplit()
+        self.students = sorted(students)
 
     def plants(self, student_name):
         student_index = self.students.index(student_name)
@@ -26,4 +23,4 @@ class Garden:
             student_plants.append(self.diagram[row][student_index * 2])
             student_plants.append(self.diagram[row][student_index * 2 + 1])
 
-        return [self.plant[p] for p in student_plants]
+        return [self.PLANTS[plant] for plant in student_plants]
