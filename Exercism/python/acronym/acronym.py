e4c54dec6d1fdbb6b40f94709390517d3cb5c5eb
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-01-19 21:54:49 +0700

    Changed: acronym working solution. It seems a little overkill.

--- Exercism/python/acronym/acronym.py
+++ Exercism/python/acronym/acronym.py
@@ -1,8 +1,12 @@
+import re
+
+
 def abbreviate(words):
+    words = re.findall(r"[\w']+", words)
     acronym = str()
 
-    for word in words.split():
-        if word[:1].isaplha():
-            acronym += ''.join(word[:1])
+    for word in words:
+        word = ''.join(e for e in word if e.isalnum())
+        acronym += ''.join(word[:1].capitalize())
 
     return acronym.upper()
