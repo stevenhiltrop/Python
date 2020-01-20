58f83a080850306b7071e76998eb75a585267ae3
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-01-20 19:38:02 +0700

    Changed: clock.py solution

--- Exercism/python/clock/clock.py
+++ Exercism/python/clock/clock.py
@@ -1,21 +1,15 @@
-from datetime import time
-
-
 class Clock:
     def __init__(self, hour, minute):
-        self.hour = (hour + int(minute / 60)) % 24
-        self.minute = minute % 60
-
-        self.clock = time(hour=self.hour, minute=self.minute)
+        self.hour, self.min = divmod((hour * 60 + minute) % (24 * 60), 60)
 
     def __repr__(self):
-        return self.clock.isoformat(timespec='minutes')
+        return f"{self.hour:02d}:{self.min:02d}"
 
     def __eq__(self, other):
-        pass
+        return self.hour == other.hour and self.min == other.min
 
     def __add__(self, minutes):
-        pass
+        return self.__class__(self.hour, self.min + minutes)
 
     def __sub__(self, minutes):
-        pass
+        return self.__class__(self.hour, self.min - minutes)
