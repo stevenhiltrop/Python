26bb9e540763db4851cd662ba6c38a001269ba23
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-01-20 18:11:45 +0700

    Changed: luhn.py

--- Exercism/python/luhn/luhn.py
+++ Exercism/python/luhn/luhn.py
@@ -1,22 +1,15 @@
 class Luhn:
     def __init__(self, card_num):
         self.card_number = card_num.replace(' ', '')
-        self.num_digits = len(self.card_number)
-        self.odd_even = self.num_digits & 1
-        self.sum = 0
+
+    def checksum(self):
+        digits = list(map(int, self.card_number))
+        odd_sum = sum(digits[-1::-2])
+        even_sum = sum([sum(divmod(2 * d, 10)) for d in digits[-2::-2]])
+        return (odd_sum + even_sum) % 10
 
     def valid(self):
         if self.card_number.isdigit():
-            for count in range(0, self.num_digits):
-                digit = int(self.card_number[count])
-
-                if not ((count & 1) ^ self.odd_even):
-                    digit = digit * 2
-                if digit > 9:
-                    digit = digit - 9
-
-                self.sum = self.sum + digit
-
-            return (self.sum % 10) == 0
+            return self.checksum() == 0
         else:
             return False
