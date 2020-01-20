c4f5dec7d81fe653db94f494b9a73624e237f942
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-01-20 17:47:19 +0700

    Changed: luhn.py

--- Exercism/python/luhn/luhn.py
+++ Exercism/python/luhn/luhn.py
@@ -1,17 +1,22 @@
-# TODO: Index the number so we can put the doubles back in the original card_num
 class Luhn:
     def __init__(self, card_num):
         self.card_number = card_num.replace(' ', '')
+        self.num_digits = len(self.card_number)
+        self.odd_even = self.num_digits & 1
+        self.sum = 0
 
     def valid(self):
         if self.card_number.isdigit():
-            numbers = [int(number) for number in self.card_number]
+            for count in range(0, self.num_digits):
+                digit = int(self.card_number[count])
 
-            for number in numbers[::2]:
-                index = numbers.index(number)
-                double = int(number) * 2
-                numbers[index] = double - 9 if double > 9 else double
+                if not ((count & 1) ^ self.odd_even):
+                    digit = digit * 2
+                if digit > 9:
+                    digit = digit - 9
 
-            return sum(numbers) % 10 == 0
+                self.sum = self.sum + digit
+
+            return (self.sum % 10) == 0
         else:
             return False
