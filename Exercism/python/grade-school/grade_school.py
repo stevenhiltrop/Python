6c0573f65531ffb12b18cce41c3830bd6b852fba
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-01-20 14:23:20 +0700

    Changed: grade_school.py using collections

--- Exercism/python/grade-school/grade_school.py
+++ Exercism/python/grade-school/grade_school.py
@@ -1,28 +1,17 @@
+import collections
+
+
 class School:
-    def __init__(self):
-        self.school = list()
+    def __init__(self, school=""):
+        self.school = collections.defaultdict(list)
+        self.name = school
 
     def add_student(self, name, grade):
-        self.school.append((name, grade))
+        self.school[grade].append(name)
 
     def roster(self):
-        students = list()
-
-        for student in self.school:
-            students.append(student[0])
-
-        self.school.sort(key=comparator)
-        return students
+        school = [self.grade(grade) for grade in sorted(self.school.keys())]
+        return [child for _class in school for child in _class]
 
     def grade(self, grade_number):
-        students = list()
-
-        for student in self.school:
-            if grade_number in student:
-                students.append(student[0])
-
-        return sorted(students)
-
-
-def comparator(tuple_element):
-    return tuple_element[1]
+        return sorted(self.school[grade_number])
