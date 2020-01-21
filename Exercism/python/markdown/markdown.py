44641c6de59c630b85eb367a6e5dbf4e0526c5b8
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-01-21 21:07:03 +0700

    Changed: markdown.py solution

--- Exercism/python/markdown/markdown.py
+++ Exercism/python/markdown/markdown.py
@@ -2,13 +2,13 @@ import re
 
 
 def parse(markdown):
-    s = markdown
-    s = re.sub(r'__([^\n]+?)__', r'<strong>\1</strong>', s)
-    s = re.sub(r'_([^\n]+?)_', r'<em>\1</em>', s)
-    s = re.sub(r'^\* (.*?$)', r'<li>\1</li>', s, flags=re.M)
-    s = re.sub(r'(<li>.*</li>)', r'<ul>\1</ul>', s, flags=re.S)
+    string = markdown
+    string = re.sub(r'__([^\n]+?)__', r'<strong>\1</strong>', string)
+    string = re.sub(r'_([^\n]+?)_', r'<em>\1</em>', string)
+    string = re.sub(r'^\* (.*?$)', r'<li>\1</li>', string, flags=re.M)
+    string = re.sub(r'(<li>.*</li>)', r'<ul>\1</ul>', string, flags=re.S)
     for i in range(6, 0, -1):
-        s = re.sub(r'^{} (.*?$)'.format('#' * i), r'<h{0}>\1</h{0}>'.format(i), s, flags=re.M)
-    s = re.sub(r'^(?!<[hlu])(.*?$)', r'<p>\1</p>', s, flags=re.M)
-    s = re.sub(r'\n', '', s)
-    return s
+        string = re.sub(r'^{} (.*?$)'.format('#' * i), r'<h{0}>\1</h{0}>'.format(i), string, flags=re.M)
+    string = re.sub(r'^(?!<[hlu])(.*?$)', r'<p>\1</p>', string, flags=re.M)
+    string = re.sub(r'\n', '', string)
+    return string
