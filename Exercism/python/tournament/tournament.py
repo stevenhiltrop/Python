e1d283a0acf0f3354ce9c533dac1741a441d7556
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-01-22 17:38:18 +0700

    Added: tournament.py

---
+++ Exercism/python/tournament/tournament.py
@@ -0,0 +1,74 @@
+from collections import OrderedDict
+
+
+class Team:
+    def __init__(self, team):
+        self.name = team
+        self.played = 0
+        self.win = 0
+        self.draw = 0
+        self.loss = 0
+        self.points = 0
+
+    def score(self, result):
+        if result == "win":
+            self.won()
+        elif result == "loss":
+            self.lost()
+        elif result == "draw":
+            self.tied()
+        else:
+            raise ValueError("Only 'win', 'loss' or 'draw' values are allowed")
+
+    def won(self):
+        self.played += 1
+        self.win += 1
+        self.points += 3
+
+    def lost(self):
+        self.played += 1
+        self.loss += 1
+
+    def tied(self):
+        self.played += 1
+        self.draw += 1
+        self.points += 1
+
+
+def create_table(teams):
+    table = ["Team                           | MP |  W |  D |  L |  P"]
+    for team in teams.values():
+        table.append(team.name.ljust(31) + '|  ' + ' |  '.join(
+            str(team.played) +
+            str(team.win) +
+            str(team.draw) +
+            str(team.loss) +
+            str(team.points)
+        ))
+    return table
+
+
+def tally(rows):
+    teams = dict()
+    outcome_mapping = {
+        "team1": "team2",
+        "win": "loss",
+        "loss": "win",
+        "draw": "draw"
+    }
+
+    for row in rows:
+        team1, team2, outcome = row.split(';')  # "Allegoric Alaskans;Blithering Badgers;win",
+
+        if team1 not in teams:
+            teams[team1] = Team(team1)
+
+        if team2 not in teams:
+            teams[team2] = Team(team2)
+
+        teams[team1].score(outcome)
+        teams[team2].score(outcome_mapping[outcome])
+
+    table = create_table(teams)
+
+    return table
