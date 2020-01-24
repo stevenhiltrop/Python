2aa5a2ddd691a94951a8ad087896173b04fc447f
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-01-24 22:36:04 +0700

    Added: rest_api.py single user search

--- Exercism/python/rest-api/rest_api.py
+++ Exercism/python/rest-api/rest_api.py
@@ -3,21 +3,33 @@ import json
 
 class RestAPI:
     def __init__(self, database=None):
-        self.users = database
+        self.database = database
 
     def get(self, url, payload=None):
         if url == "/users":
-            return json.dumps(self.users)
+            if payload:
+                payload = json.loads(payload)
+                try:
+                    found_users = [
+                        user for user in self.database['users']
+                        for requested_user in payload['users']
+                        if user["name"] == requested_user
+                    ]
+                    return json.dumps({"users": found_users})
+                except ConnectionError as exception:
+                    raise "{1} {0}".format("Database does not exist", exception)
+            else:
+                return json.dumps(self.database)
 
     def post(self, url, payload=None):
         if url == "/add":
-            user = self.users["users"] = {
+            self.database["users"] = {
                 "name": json.loads(payload)['user'],
                 "owes": {},
                 "owed_by": {},
                 "balance": 0.0
             }
-            return json.dumps(user)
+            return json.dumps(self.get(payload=payload))
         elif url == "/iou":
             pass
 
