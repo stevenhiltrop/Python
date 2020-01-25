0983aeb4ababd4ee9d30059fac4e152db33666a2
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-01-25 12:14:36 +0700

    Changed: rest_api.py

--- Exercism/python/rest-api/rest_api.py
+++ Exercism/python/rest-api/rest_api.py
@@ -2,39 +2,56 @@ import json
 
 
 class RestAPI:
+    database = {}
+
     def __init__(self, database=None):
         self.database = database
 
     def get(self, url, payload=None):
-        if url == "/users":
-            if payload:
-                payload = json.loads(payload)
-                try:
-                    found_users = [
-                        user for user in self.database['users']
-                        for requested_user in payload['users']
-                        if user["name"] == requested_user
-                    ]
-                    return json.dumps({"users": found_users})
-                except ConnectionError as exception:
-                    raise "{1} {0}".format("Database does not exist", exception)
-            else:
-                return json.dumps(self.database)
+        if url == '/users' and payload is None:
+            return json.dumps(self.database)
+        elif url == '/users' and payload:
+            names = json.loads(payload)["users"]
+            users = {"users": list(filter(lambda user: user["name"] in names,
+                                          self.database["users"]))}
+            return json.dumps(users)
 
     def post(self, url, payload=None):
         if url == "/add":
-            self.database["users"] = {
-                "name": json.loads(payload)['user'],
-                "owes": {},
-                "owed_by": {},
-                "balance": 0.0
-            }
-            return json.dumps(self.database['users'])
+            new_user = {"name": json.loads(payload)["user"],
+                        "owes": {}, "owed_by": {}, "balance": 0.0}
+            self.database["users"].append(new_user)
+            return json.dumps(new_user)
         elif url == "/iou":
-            pass
+            iou = json.loads(payload)
+            lender = [i for i, _ in enumerate(self.database["users"]) if _['name'] == iou["lender"]][0]
+            borrower = [i for i, _ in enumerate(self.database["users"]) if _['name'] == iou["borrower"]][0]
 
-# database = {"users": []}
-# api = RestAPI(database)
-#
-# response = api.get("/users")
-# expected = {"users": []}
+            amount = float(iou["amount"])
+            self.database["users"][lender]["balance"] += amount
+            self.database["users"][borrower]["balance"] -= amount
+
+            if (iou["borrower"] in self.database["users"][lender]["owes"].keys() and
+                    iou["lender"] in self.database["users"][borrower]["owed_by"].keys()):
+                if (self.database["users"][lender]["owes"][iou["borrower"]] > amount):
+                    self.database["users"][lender]["owes"][iou["borrower"]] -= amount
+                    self.database["users"][borrower]["owed_by"][iou["lender"]] -= amount
+                else:
+                    balance = amount - self.database["users"][lender]["owes"][iou["borrower"]]
+                    self.database["users"][lender]["owes"].pop(iou["borrower"])
+                    self.database["users"][borrower]["owed_by"].pop(iou["lender"])
+                    if balance > 0:
+                        self.database["users"][lender]["owed_by"][iou["borrower"]] = balance
+                        self.database["users"][borrower]["owes"][iou["lender"]] = balance
+            elif (iou["borrower"] in self.database["users"][lender]["owed_by"].keys() and
+                  iou["lender"] in self.database["users"][borrower]["owes"].keys()):
+                self.database["users"][lender]["owed_by"][iou["borrower"]] += amount
+                self.database["users"][borrower]["owes"][iou["lender"]] += amount
+            else:
+                self.database["users"][lender]["owed_by"][iou["borrower"]] = amount
+                self.database["users"][borrower]["owes"][iou["lender"]] = amount
+
+            users = {"users": list(filter(lambda user: user["name"] in [iou["lender"], iou["borrower"]],
+                                          self.database["users"]))}
+
+            return json.dumps(users)
