e727ba4c33959a46b9fe026cf68061398ba5baf1
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-01-26 22:58:11 +0700

    Added: isbn.py

---
+++ Exercism/python/isbn-verifier/isbn_verifier.py
@@ -0,0 +1,16 @@
+def is_valid(isbn):  # "3-598-21507-A"
+    formatted_number = isbn.replace("-", "")
+
+    if len(formatted_number) != 10 or formatted_number[9].isdigit():
+        return False
+
+    check_sum = sum(int(formatted_number[i]) * (10 - i) for i in range(8))
+    check_sum %= 11
+    check_digit = 11 - check_sum
+
+    if formatted_number.endswith("X"):
+        return check_digit == 10
+    elif formatted_number[-1].isdigit():
+        return check_digit == int(formatted_number[-1])
+    else:
+        return False
