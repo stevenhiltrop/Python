973e1a034afb8917514c6c7b8526665ae10a19ca
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-01-27 17:10:11 +0700

    Added: yacht.py

---
+++ Exercism/python/yacht/yacht.py
@@ -0,0 +1,32 @@
+"""
+This exercise stub and the test suite contain several enumerated constants.
+
+Since Python 2 does not have the enum module, the idiomatic way to write
+enumerated constants has traditionally been a NAME assigned to an arbitrary,
+but unique value. An integer is traditionally used because it’s memory
+efficient.
+It is a common practice to export both constants and functions that work with
+those constants (ex. the constants in the os, subprocess and re modules).
+
+You can learn more here: https://en.wikipedia.org/wiki/Enumerated_type
+"""
+
+# Score categories.
+# Change the values as you see fit.
+YACHT = 50
+ONES = 1
+TWOS = 2
+THREES = 3
+FOURS = 4
+FIVES = 5
+SIXES = 6
+FULL_HOUSE = None
+FOUR_OF_A_KIND = None
+LITTLE_STRAIGHT = 30
+BIG_STRAIGHT = 30
+CHOICE = sum
+
+
+def score(dice, category):
+    # YACHT = (number == dice[0] for number in dice) else 0
+    pass
