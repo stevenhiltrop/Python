4df5247614d865af51ccdc3eaaad31dd5a1f18b7
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-01-27 22:53:02 +0700

    Added: run_length_encoding.py solution

--- Exercism/python/run-length-encoding/run_length_encoding.py
+++ Exercism/python/run-length-encoding/run_length_encoding.py
@@ -1,4 +1,5 @@
 from itertools import groupby
+from re import sub
 
 
 def decode(string: str) -> str:
@@ -11,13 +12,7 @@ def decode(string: str) -> str:
     :return:
     result: str
     """
-    result = ""
-
-    for index, char in enumerate(string):
-        if char.isdigit():
-            result += int(char) * string[index + 1]
-
-    return "".join(result)
+    return sub(r'(\d+)(\D)', lambda x: x.group(2) * int(x.group(1)), string)
 
 
 def encode(string: str) -> str:
@@ -30,14 +25,4 @@ def encode(string: str) -> str:
         :return:
         result: str
         """
-    result = ""
-
-    for k, i in groupby(string):
-        run = list(i)
-
-        if len(run) > 1:
-            result += "{0}{1}".format(len(run), k)
-        else:
-            result += run
-
-    return "".join(result)
+    return sub(r'(.)\1+', lambda x: str(len(x.group(0))) + x.group(1), string)
