296b120b4dc55279f0d409f45f4fe880cf9efe90
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-01-27 17:15:33 +0700

    Added: yacht.py solution

--- Exercism/python/yacht/yacht.py
+++ Exercism/python/yacht/yacht.py
@@ -13,20 +13,22 @@ You can learn more here: https://en.wikipedia.org/wiki/Enumerated_type
 
 # Score categories.
 # Change the values as you see fit.
-YACHT = 50
-ONES = 1
-TWOS = 2
-THREES = 3
-FOURS = 4
-FIVES = 5
-SIXES = 6
-FULL_HOUSE = None
-FOUR_OF_A_KIND = None
-LITTLE_STRAIGHT = 30
-BIG_STRAIGHT = 30
+YACHT = lambda d: 50 if len(set(d)) == 1 else 0
+ONES = lambda d: sum(x for x in d if x == 1)
+TWOS = lambda d: sum(x for x in d if x == 2)
+THREES = lambda d: sum(x for x in d if x == 3)
+FOURS = lambda d: sum(x for x in d if x == 4)
+FIVES = lambda d: sum(x for x in d if x == 5)
+SIXES = lambda d: sum(x for x in d if x == 6)
+FULL_HOUSE = lambda d: sum(d) if len(set(d)) == 2 and any(d.count(x) == 3 for x in set(d)) else 0
+FOUR_OF_A_KIND = lambda d: sum(x * 4 for x in set(d) if d.count(x) > 3)
+LITTLE_STRAIGHT = lambda d: 30 if sum(d) == 15 and len(set(d)) == 5 else 0
+BIG_STRAIGHT = lambda d: 30 if sum(d) == 20 and len(set(d)) == 5 else 0
 CHOICE = sum
 
 
 def score(dice, category):
-    # YACHT = (number == dice[0] for number in dice) else 0
-    pass
+    if any(not 0 < x < 7 for x in dice):
+        raise ValueError("Invalid dice {}".format(dice))
+
+    return category(dice)
