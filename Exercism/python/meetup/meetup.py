1781be45d42ba1b5bdb9b131907cab9c5365b2d0
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-01-31 20:32:27 +0700

    Added: meetup.py commentary

--- Exercism/python/meetup/meetup.py
+++ Exercism/python/meetup/meetup.py
@@ -1,22 +1,45 @@
-from datetime import date
+import calendar
+from datetime import date, timedelta
 
 
-def meetup(year, month, week_of_month, day_of_week):  # Monday = 0 -> Sunday = 6
-    days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+def meetup(year, month, week, day_of_week):
+    """
+    Calculate the date of the meetup
 
-    week = {
-        "teenth": 0,
-        "1st": 0,
-        "2nd": 7,
-        "3rd": 14,
-        "4th": 21,
-        "5th": 28,
-        "last": 28
-    }[week_of_month]
+    :param
+    year: int
+    month: int
+    week: str
+        "teenth", "1st", "2nd", "3rd", "4th", "5th", "last"
+    day_of_week: str
+        "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
 
-    day = week + days.index(day_of_week)
+    :return:
+    datetime.date
+    """
+    day = {
+        "Monday": calendar.MONDAY,
+        "Tuesday": calendar.TUESDAY,
+        "Wednesday": calendar.WEDNESDAY,
+        "Thursday": calendar.THURSDAY,
+        "Friday": calendar.FRIDAY,
+        "Saturday": calendar.SATURDAY,
+        "Sunday": calendar.SUNDAY
+    }[day_of_week]
 
-    return date(year, month, day)
+    if week[0].isdigit():
+        month_range = calendar.monthrange(year, month, w=int(week[0]))
+    elif week == "last":
+        month_range = calendar.monthrange(year, month, w=month_range[1])
+    elif week == "teenth":
+        pass
+    else:
+        raise MeetupDayException
+
+    date_corrected = date(year, month, month_range[1])
+    delta = (day - month_range[0]) % 7
+
+    return date_corrected + timedelta(days=delta)
 
 
 class MeetupDayException(Exception):
