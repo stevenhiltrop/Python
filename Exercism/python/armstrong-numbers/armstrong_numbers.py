d381e2fc7ca14fb4edaf1f14ce16953d5787c4e2
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-02-01 14:50:57 +0700

    Added: armstrong_numbers.py commentary

--- Exercism/python/armstrong-numbers/armstrong_numbers.py
+++ Exercism/python/armstrong-numbers/armstrong_numbers.py
@@ -1,10 +1,24 @@
 def is_armstrong_number(number):
+    """
+    Check if a number is an armstrong number
+
+    :param
+    number: int
+
+    :return:
+    bool
+        [True, False]
+    """
+    # Pythonic way
+
     numbers = list(str(number))
     total = 0
     for digit in numbers:
         total += pow(int(digit), len(numbers))
 
     return True if total == number else False
+
+    # Non-pythonic way
 
     # numbers = list(str(number))
     # armstrong = lambda digit: pow(int(digit), len(numbers))
