ae36a0c5778d133a11381917fce73135e1c8a69d
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-02-05 16:48:26 +0700

    Added: difference-of-squares.py commentary

--- Exercism/python/difference-of-squares/difference_of_squares.py
+++ Exercism/python/difference-of-squares/difference_of_squares.py
@@ -1,10 +1,39 @@
-def square_of_sum(number):  # (1 + 2 + ... + 10)² = 55² = 3025.
-    return pow(sum(range(number+1)), 2)
+def square_of_sum(number):
+    """
+    Square the total of each number starting 0.
+    Example: (1 + 2 + ... + 10)² = 55² = 3025.
+
+    :param
+    number: int
+
+    :return:
+    square_of_sum: int
+    """
+    return pow(sum(range(number + 1)), 2)
 
 
-def sum_of_squares(number):  # 1² + 2² + ... + 10² = 385.
-    return sum([pow(num, 2) for num in range(number+1)])
+def sum_of_squares(number):
+    """
+    Square of each number added together starting 0.
+    Example: 1² + 2² + ... + 10² = 385.
+
+    :param
+    number: int
+
+    :return:
+    sum_of_squares: int
+    """
+    return sum([pow(num, 2) for num in range(number + 1)])
 
 
-def difference_of_squares(number):  # 3025 - 385 = 2640.
+def difference_of_squares(number):
+    """
+    Difference between square_of_sum and sum_of_squares.
+
+    :param
+    number: int
+
+    :return:
+    difference_of_squares: int
+    """
     return square_of_sum(number) - sum_of_squares(number)
