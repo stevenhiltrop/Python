3baa1ab6a192dbbbed6911a47757cef52c2937c2
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-02-05 15:48:06 +0700

    Changed: rotational_cipher.py functionality

--- Exercism/python/rotational-cipher/rotational_cipher.py
+++ Exercism/python/rotational-cipher/rotational_cipher.py
@@ -1,25 +1,2 @@
 def rotate(text, key):
-    result = ""
-
-    # Loop over characters.
-    for char in text:
-        # Convert to number with ord.
-        char_byte = ord(char)
-
-        # Shift number back or forward.
-        if char_byte >= ord('a') and char_byte <= ord('z'):
-            if char_byte > ord('m'):
-                char_byte -= key
-            else:
-                char_byte += key
-        elif char_byte >= ord('A') and char_byte <= ord('Z'):
-            if char_byte > ord('M'):
-                char_byte -= key
-            else:
-                char_byte += key
-
-        # Append to result.
-        result += chr(char_byte)
-
-    # Return transformation.
-    return result
+    return ''.join(map(lambda c: chr(ord('a') + (ord(c) - ord('a') + key) % 26), text))
