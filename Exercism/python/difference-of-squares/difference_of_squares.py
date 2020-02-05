a3818291c2f74d355065ea196eb8c6f32a57a181
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-02-05 16:43:49 +0700

    Added: difference-of-squares.py functionality

--- Exercism/python/difference-of-squares/difference_of_squares.py
+++ Exercism/python/difference-of-squares/difference_of_squares.py
@@ -1,10 +1,10 @@
 def square_of_sum(number):  # (1 + 2 + ... + 10)² = 55² = 3025.
-    pass
+    return pow(sum(range(number+1)), 2)
 
 
 def sum_of_squares(number):  # 1² + 2² + ... + 10² = 385.
-    pass
+    return sum([pow(num, 2) for num in range(number+1)])
 
 
 def difference_of_squares(number):  # 3025 - 385 = 2640.
-    pass
+    return square_of_sum(number) - sum_of_squares(number)
