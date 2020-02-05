f3b0fd56164fd0d29e36b5dcbfb8dc9bf92e8b84
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-02-05 17:19:36 +0700

    Added: anagram.py

---
+++ Exercism/python/anagram/anagram.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+def find_anagrams(word: str, candidates: List[str]) -> List[str]:
+    """
+    Check if there are anagrams for the given candidates
+
+    :param
+    word: str
+    candidates: List[str]
+
+    :return:
+    anagrams: List[str
+    """
+    matches = list()
+
+    for item in candidates:
+        if item.lower() == word.lower():
+            pass
+        elif sorted(item.lower()) == sorted(word.lower()):
+            matches.append(item)
+
+    return matches
