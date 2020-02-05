76492ed56d5e4faed18bef0c1c6743d03c222108
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-02-05 16:25:32 +0700

    Added: difference-of-squares.py

---
+++ Exercism/python/difference-of-squares/difference_of_squares.py
@@ -0,0 +1,10 @@
+def square_of_sum(number):  # (1 + 2 + ... + 10)² = 55² = 3025.
+    pass
+
+
+def sum_of_squares(number):  # 1² + 2² + ... + 10² = 385.
+    pass
+
+
+def difference_of_squares(number):  # 3025 - 385 = 2640.
+    pass
