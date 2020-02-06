1bc9a4a7dd595bd60387d60ff00fbeac2f59bf46
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-02-06 17:41:47 +0700

    Added: series.py solution

--- Exercism/python/series/series.py
+++ Exercism/python/series/series.py
@@ -1,2 +1,18 @@
-def slices(series, length):
-    return list(series[x:length + x] for x in range(len(series)) if len(series[x:length + x]) == length)
+from typing import List
+
+
+def slices(series: str, length: int) -> List[str]:
+    """
+    Get slices for a series according to the given length
+
+    :param
+    series: str
+    length: int
+
+    :return:
+    List[str]
+    """
+    if length > len(series) or length <= 0 or not series:
+        raise ValueError(".+")
+    else:
+        return list(series[x:length + x] for x in range(len(series)) if len(series[x:length + x]) == length)
