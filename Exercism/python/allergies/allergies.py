0ff7cd76d1fc9300f1f0d7744be8d830db4f33b8
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-02-06 16:29:36 +0700

    Added: allergies.py solution

--- Exercism/python/allergies/allergies.py
+++ Exercism/python/allergies/allergies.py
@@ -1,22 +1,40 @@
-ALLERGIES = {"eggs": 1,
-             "peanuts": 2,
-             "shellfish": 4,
-             "strawberries": 8,
-             "tomatoes": 16,
-             "chocolate": 32,
-             "pollen": 64,
-             "cats": 128}
-
-
 class Allergies:
+    """
+    Contains a score and allergies
 
-    def __init__(self, score):
+    :param:
+    score: int
+    allergies: dict
+    """
+    def __init__(self, score: int):
         self.score = score
-        self.__lst = [key for key in ALLERGIES if self.allergic_to(key)]
+        self.allergies = {"eggs": 1,
+                          "peanuts": 2,
+                          "shellfish": 4,
+                          "strawberries": 8,
+                          "tomatoes": 16,
+                          "chocolate": 32,
+                          "pollen": 64,
+                          "cats": 128}
 
     def allergic_to(self, item):
-        return bool(self.score & ALLERGIES.get(item, 0))
+        """
+        Check if you are allergic to item
+
+        :param
+        item: str
+
+        :return:
+        bool
+        """
+        return bool(self.score & self.allergies.get(item, 0))
 
     @property
     def lst(self):
-        return self.__lst
+        """
+        List of items you are allergic to
+
+        :return:
+        list
+        """
+        return [key for key in self.allergies if self.allergic_to(key)]
