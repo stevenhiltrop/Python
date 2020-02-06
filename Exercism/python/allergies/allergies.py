0243da50cad85021dfaa5383f69f1ef4c97eceb5
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-02-06 16:03:59 +0700

    Added: allergies.py solution

--- Exercism/python/allergies/allergies.py
+++ Exercism/python/allergies/allergies.py
@@ -1,28 +1,22 @@
+ALLERGIES = {"eggs": 1,
+             "peanuts": 2,
+             "shellfish": 4,
+             "strawberries": 8,
+             "tomatoes": 16,
+             "chocolate": 32,
+             "pollen": 64,
+             "cats": 128}
+
+
 class Allergies:
 
     def __init__(self, score):
-        self.allergy = score
-        self.allergies = {
-            "eggs": 1,
-            "peanuts": 2,
-            "shellfish": 4,
-            "strawberries": 8,
-            "tomatoes": 16,
-            "chocolate": 32,
-            "pollen": 64,
-            "cats": 128
-        }
+        self.score = score
+        self.__lst = [key for key in ALLERGIES if self.allergic_to(key)]
 
     def allergic_to(self, item):
-        return True if self.allergy >= self.allergies[item] else False
+        return bool(self.score & ALLERGIES.get(item, 0))
 
     @property
     def lst(self):
-        allergies = list()
-
-        for allergy, score in reversed(self.allergies.items()):
-            if score <= self.allergy:
-                allergies.append(allergy)
-                self.allergy -= score
-
-        return allergies
+        return self.__lst
