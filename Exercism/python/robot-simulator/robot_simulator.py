c96897cb8511c8545c82909a0f72801fa87f26dd
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-02-09 19:05:09 +0700

    Added: robot_simulator.py commentary

--- Exercism/python/robot-simulator/robot_simulator.py
+++ Exercism/python/robot-simulator/robot_simulator.py
@@ -7,16 +7,35 @@ SOUTH = 3
 
 
 class Robot:
+    """
+    A robot that can move left, right and advance the position it is facing.
+    """
     def __init__(self, direction=NORTH, x=0, y=0):
-        self.position_x = x
-        self.position_y = y
+        """
+        :param
+        direction: [NORTH, EAST, SOUTH, WEST]
+        x: int
+        y: int
+        """
+        self._position_x = x
+        self._position_y = y
         self.direction = direction
 
     @property
     def coordinates(self):
-        return tuple((self.position_x, self.position_y))
+        """
+        :return:
+        coordinates: tuple
+            (2, 3)
+        """
+        return tuple((self._position_x, self._position_y))
 
-    def move(self, movement):
+    def move(self, movement: str):
+        """
+        :param
+        movement: str
+            'A', 'R', 'L'
+        """
         for direction in movement:
             if direction == 'R':
                 self.direction = (self.direction + 1) % 4
@@ -24,12 +43,12 @@ class Robot:
                 self.direction = (self.direction - 1) % 4
             elif direction == 'A':
                 if self.direction is NORTH:
-                    self.position_y += 1
+                    self._position_y += 1
                 elif self.direction is SOUTH:
-                    self.position_y -= 1
+                    self._position_y -= 1
                 elif self.direction is EAST:
-                    self.position_x += 1
+                    self._position_x += 1
                 elif self.direction is WEST:
-                    self.position_x -= 1
+                    self._position_x -= 1
             else:
                 raise ValueError("Wrong movement entered. Use R/A/L.")
