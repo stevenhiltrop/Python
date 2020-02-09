f55d9f11ab4637e4e430e0faf8edf13dbc716a27
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-02-09 18:46:17 +0700

    Added: robot_simulator.py solution

--- Exercism/python/robot-simulator/robot_simulator.py
+++ Exercism/python/robot-simulator/robot_simulator.py
@@ -1,9 +1,9 @@
 # Globals for the directions
 # Change the values as you see fit
-EAST = 1
+EAST = 2
 NORTH = 1
-WEST = -1
-SOUTH = -1
+WEST = 0
+SOUTH = 3
 
 
 class Robot:
@@ -11,11 +11,25 @@ class Robot:
         self.position_x = x
         self.position_y = y
         self.direction = direction
-        self.movement = None
 
     @property
     def coordinates(self):
         return tuple((self.position_x, self.position_y))
 
-    def move(self, direction):
-        pass
+    def move(self, movement):
+        for direction in movement:
+            if direction == 'R':
+                self.direction = (self.direction + 1) % 4
+            elif direction == 'L':
+                self.direction = (self.direction - 1) % 4
+            elif direction == 'A':
+                if self.direction is NORTH:
+                    self.position_y += 1
+                elif self.direction is SOUTH:
+                    self.position_y -= 1
+                elif self.direction is EAST:
+                    self.position_x += 1
+                elif self.direction is WEST:
+                    self.position_x -= 1
+            else:
+                raise ValueError("Wrong movement entered. Use R/A/L.")
