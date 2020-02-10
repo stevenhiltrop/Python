604550b7b4ed5de97de73d503d76cec425981ee4
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-02-10 09:34:13 +0700

    Changed: sieve.py functionality, saving progress.

--- Exercism/python/sieve/sieve.py
+++ Exercism/python/sieve/sieve.py
@@ -1,2 +1,24 @@
-def primes(limit: int):
-    pass
+def primes(limit: int) -> list:
+    """
+    Get a list of prime numbers for the given limit using Sieve of Eratosthenes
+
+    :param:
+    limit: int
+
+    :return:
+    primes: list
+    """
+    if limit >= 2:
+        a = [True] * limit
+        a[0] = a[1] = False
+        prime_numbers = list()
+
+        for (i, is_prime) in enumerate(a):
+            if is_prime:
+                prime_numbers.append(i)
+                for n in range(i * i, limit, i):  # Mark factors non-prime
+                    a[n] = False
+
+        return prime_numbers
+    else:
+        return list()
