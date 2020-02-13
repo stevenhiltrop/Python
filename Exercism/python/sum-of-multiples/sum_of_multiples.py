e7356226af9da435039ee3de6ee737c204021d06
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-02-13 16:25:14 +0700

    Added: sum_of_multiples.py commentary

--- Exercism/python/sum-of-multiples/sum_of_multiples.py
+++ Exercism/python/sum-of-multiples/sum_of_multiples.py
@@ -1,9 +1,23 @@
-def sum_of_multiples(limit, multiples):  # 7, [3]), 9 --> 3 + 6 = 9
+from typing import List
+
+
+def sum_of_multiples(limit: int, multiples: List[int]) -> int:
+    """
+    Return the sum of all the unique multiples of particular numbers up to but not including that number.
+
+    :param:
+    limit: int
+    multiples: list
+
+    :return:
+    sum_of_multiples: int
+    """
     multipliers = list()
 
     for multiplier in multiples:
-        for number in range(1, limit):
-            if number % multiplier == 0:
-                multipliers.append(number)
+        if multiplier != 0:
+            for number in range(1, limit):
+                if number % multiplier == 0:
+                    multipliers.append(number)
 
     return sum(set(multipliers))
