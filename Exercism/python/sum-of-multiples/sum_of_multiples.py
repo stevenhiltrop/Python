4bfbf64c1cd7320ebf948fa88bfff123b3b8ab51
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-02-13 16:12:19 +0700

    Added: sum_of_multiples.py functionality

--- Exercism/python/sum-of-multiples/sum_of_multiples.py
+++ Exercism/python/sum-of-multiples/sum_of_multiples.py
@@ -1,2 +1,9 @@
-def sum_of_multiples(limit, multiples):
-    pass
+def sum_of_multiples(limit, multiples):  # 7, [3]), 9 --> 3 + 6 = 9
+    multipliers = list()
+
+    for multiplier in multiples:
+        for number in range(1, limit):
+            if number % multiplier == 0:
+                multipliers.append(number)
+
+    return sum(set(multipliers))
