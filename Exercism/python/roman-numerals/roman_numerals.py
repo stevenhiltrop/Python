70a67be99335f97e5c1cdfb5ef1e4b2cac3bbab8
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-02-15 16:08:24 +0700

    Added: roman_numerals.py

---
+++ Exercism/python/roman-numerals/roman_numerals.py
@@ -0,0 +1,18 @@
+def roman(number: int) -> str:
+    """
+    Convert an integer to a Roman numeral
+
+    :param:
+    number: int
+
+    :return:
+    result: str
+    """
+    ints = (1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1)
+    nums = ('M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I')
+    result = []
+    for i in range(len(ints)):
+        count = int(number / ints[i])
+        result.append(nums[i] * count)
+        number -= ints[i] * count
+    return ''.join(result)
