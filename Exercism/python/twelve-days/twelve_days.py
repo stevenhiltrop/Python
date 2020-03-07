21369307e632432129a4b1330c6a8c232d5ee559
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-03-07 13:51:37 +0700

    Changed: twelve_days.py solution

--- Exercism/python/twelve-days/twelve_days.py
+++ Exercism/python/twelve-days/twelve_days.py
@@ -1,36 +1,28 @@
-def recite(start_verse, end_verse):
-    days = {
-        1: "first",
-        2: "second",
-        3: "third",
-        4: "fourth",
-        5: "fifth",
-        6: "sixth",
-        7: "seventh",
-        8: "eighth",
-        9: "ninth",
-        10: "tenth",
-        11: "eleventh",
-        12: "twelfth"
-    }
-    christmas_presents = [
-        "and a Partridge in a Pear Tree.",
-        "two Turtle Doves",
-        "three French Hens",
-        "four Calling Birds",
-        "five Gold Rings",
-        "six Geese-a-Laying",
-        "seven Swans-a-Swimming",
-        "eight Maids-a-Milking",
-        "nine Ladies Dancing",
-        "ten Lords-a-Leaping",
-        "eleven Pipers Piping",
-        "twelve Drummers Drumming"
-    ]
+from typing import List
 
-    if end_verse > 1:
-        presents = ', '.join(present for present in christmas_presents[end_verse - 1::-1])
-    else:
-        presents = christmas_presents[0].replace('and ', '')
+verses = {1: [' first ', "and a Partridge in a Pear Tree."],
+          2: [' second ', "two Turtle Doves, "],
+          3: [' third ', "three French Hens, "],
+          4: [' fourth ', "four Calling Birds, "],
+          5: [' fifth ', "five Gold Rings, "],
+          6: [' sixth ', "six Geese-a-Laying, "],
+          7: [' seventh ', "seven Swans-a-Swimming, "],
+          8: [' eighth ', "eight Maids-a-Milking, "],
+          9: [' ninth ', "nine Ladies Dancing, "],
+          10: [' tenth ', "ten Lords-a-Leaping, "],
+          11: [' eleventh ', "eleven Pipers Piping, "],
+          12: [' twelfth ', "twelve Drummers Drumming, "]}
 
-    return "On the {0} day of Christmas my true love gave to me: {1}".format(days[start_verse], presents)
+
+def recite_verse(verse_num: int) -> str:
+    output = f'On the{verses[verse_num][0]}day of Christmas my true love gave to me: '
+    for i in range(verse_num, 0, -1):
+        output += f'{verses[i][1]}'
+    return output.replace('and ', '') if verse_num == 1 else output
+
+
+def recite(start_verse: int, end_verse: int) -> List[str]:
+    output = []
+    for n in range(start_verse, end_verse + 1):
+        output.append(recite_verse(n))
+    return output
