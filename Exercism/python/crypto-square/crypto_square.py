dca337445ec30abf1e075cd13857fbbe432369dc
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-03-07 13:48:14 +0700

    Changed: crypto_square.py solution

--- Exercism/python/crypto-square/crypto_square.py
+++ Exercism/python/crypto-square/crypto_square.py
@@ -1,25 +1,22 @@
-from math import ceil, sqrt
-from string import punctuation
+from math import sqrt, ceil
 
 
-def normalize(plain_text):
-    plain_text = plain_text.lower()
-    plain_text = plain_text.translate(str.maketrans('', '', punctuation))
-    plain_text = plain_text.replace(' ', '')
-    return plain_text
+def cipher_text(plain_text: str) -> str:
+    """
+    Make a secret message
 
+    :param
+    plain_text: str
 
-def cipher_text(plain_text):
-    text = normalize(plain_text)
-    if len(text) == 1 or not text:
+    :return:
+    secret_message = str
+    """
+    text = "".join(char for char in plain_text.lower() if char.isalnum())
+
+    if (text_len := len(text)) <= 1:
         return text
-    else:
-        rectangle_size = int(ceil(len(text)) / ceil(sqrt(len(text))))
-        rectangle = [text[i:i + rectangle_size] for i in range(0, len(text), rectangle_size)]
-        result = [str()] * rectangle_size
 
-        for index in range(rectangle_size):
-            for word in rectangle:
-                result[index] = result[index] + word[index]
+    ceil_size = ceil(sqrt(text_len))
+    rectangle = [text[i: i + ceil_size].ljust(ceil_size) for i in range(0, text_len, ceil_size)]
 
-        return ' '.join(result)
+    return " ".join("".join(row[i] for row in rectangle) for i in range(ceil_size))
