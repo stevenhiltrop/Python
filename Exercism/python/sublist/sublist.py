26fed22caf15f90c164a698bef1c6aa0e267819e
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-03-07 14:25:41 +0700

    Changed: sublist.py functionality Added: commentary + typing

--- Exercism/python/sublist/sublist.py
+++ Exercism/python/sublist/sublist.py
@@ -4,7 +4,17 @@ EQUAL = None
 UNEQUAL = None
 
 
-def sublist(list_one, list_two):
+def sublist(list_one: list, list_two: list) -> [SUPERLIST, SUBLIST, EQUAL, UNEQUAL]:
+    """
+    Check if the given lists are sublist, superlist, equal lists or is unequal from eachother.
+
+    :param
+    list_one: list
+    list_two: list
+
+    :return:
+    list_type: bool
+    """
     if all(item in list_two for item in list_one):
         return SUBLIST
     elif all(item in list_one for item in list_two):
