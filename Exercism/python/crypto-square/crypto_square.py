cfce721830e04a8fa28b4d16bf81e1284ffeb915
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-03-07 13:37:57 +0700

    Changed: crypto_square.py functionality

--- Exercism/python/crypto-square/crypto_square.py
+++ Exercism/python/crypto-square/crypto_square.py
@@ -1,19 +1,25 @@
-import string
+from math import ceil, sqrt
+from string import punctuation
 
 
 def normalize(plain_text):
     plain_text = plain_text.lower()
-    plain_text = plain_text.translate(str.maketrans('', '', string.punctuation))
+    plain_text = plain_text.translate(str.maketrans('', '', punctuation))
     plain_text = plain_text.replace(' ', '')
     return plain_text
 
 
 def cipher_text(plain_text):
     text = normalize(plain_text)
-    if len(text) == 1:
+    if len(text) == 1 or not text:
         return text
     else:
-        square = tuple()
-        for count in range(len(text + 1)):
-            pass
-# Then, the normalized characters are broken into rows. These rows can be regarded as forming a rectangle when printed with intervening newlines.
+        rectangle_size = int(ceil(len(text)) / ceil(sqrt(len(text))))
+        rectangle = [text[i:i + rectangle_size] for i in range(0, len(text), rectangle_size)]
+        result = [str()] * rectangle_size
+
+        for index in range(rectangle_size):
+            for word in rectangle:
+                result[index] = result[index] + word[index]
+
+        return ' '.join(result)
--- Trading/Newsscraper/scraper.py
+++ Trading/Newsscraper/scraper.py
@@ -1,8 +1,33 @@
 # -*- coding: utf-8 -*-
-import urllib.request
 import ssl
-import certifi
+import urllib.request
 
-quote_page = 'http://www.bloomberg.com/quote/SPX:IND'
-with urllib.request.urlopen(quote_page, context=ssl.create_default_context(cafile=certifi.where())) as response:
-    html = response.read()
+import certifi
+from bs4 import BeautifulSoup
+
+tickers = list()
+# TODO 1. Get all earningreports
+source = 'https://earningswhispers.com/calendar'
+with urllib.request.urlopen(source, context=ssl.create_default_context(cafile=certifi.where())) as response:
+    page = response.read()
+
+soup = BeautifulSoup(page, 'html.parser')
+whisper_tickers = soup.find_all('div', attrs={'class': 'ticker'})
+
+for ticker in whisper_tickers:
+    tickers.append(ticker.text)
+
+####################################################################################
+
+source = 'https://www.marketwatch.com/tools/earningscalendar'
+with urllib.request.urlopen(source, context=ssl.create_default_context(cafile=certifi.where())) as response:
+    page = response.read()
+
+soup = BeautifulSoup(page, 'html.parser')
+marketwatch_tickers = soup.find_all('td')
+
+for ticker in marketwatch_tickers:
+    if ticker.text.isupper():
+        tickers.append(ticker.text)
+
+# TODO 2. Check if they have enough volume
