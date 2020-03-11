e7ab35844d8c930ad9f350638b2e53058da1e0b0
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-03-11 16:41:55 +0700

    Added: pythoagorean_triplet.py

--- Exercism/python/pythagorean-triplet/pythagorean_triplet.py
+++ Exercism/python/pythagorean-triplet/pythagorean_triplet.py
@@ -1,5 +1,11 @@
+from math import sqrt
+
+
 def triplets_with_sum(number):
-    pass
+    divisors = [num for num in range(1, number + 1) if number % num == 0]
+    middle_element = int(len(divisors) / 2)
+    a, b = pow(middle_element, 2), pow(middle_element + 1, 2)
+    return a + b if is_triplet([a, b, a + b]) else list()
 
 
 def triplets_in_range(start, end):
@@ -7,4 +13,5 @@ def triplets_in_range(start, end):
 
 
 def is_triplet(triplet):
-    pass
+    root = sqrt(triplet[0] + triplet[1])
+    return int(root + 0.5) ** 2 == triplet[2]
