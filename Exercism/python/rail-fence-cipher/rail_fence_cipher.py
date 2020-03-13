12f311931a047fd8e94a77c46ab4f9d91b1fc830
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-03-13 18:00:25 +0700

    Added: rail_fence_cipher.py solution

--- Exercism/python/rail-fence-cipher/rail_fence_cipher.py
+++ Exercism/python/rail-fence-cipher/rail_fence_cipher.py
@@ -1,63 +1,48 @@
-def encode(message, rails):
-    rail = [['\n' for i in range(len(message))]
-            for j in range(rails)]
-    dir_down = False
-    row, col = 0, 0
-
-    for i in range(len(message)):
-        if (row == 0) or (row == rails - 1):
-            dir_down = not dir_down
-
-        rail[row][col] = message[i]
-        col += 1
-        if dir_down:
-            row += 1
-        else:
-            row -= 1
-    result = []
-    for i in range(rails):
-        for j in range(len(message)):
-            if rail[i][j] != '\n':
-                result.append(rail[i][j])
-    return "".join(result)
+from itertools import cycle
 
 
-def decode(encoded_message, rails):
-    rail = [['\n' for i in range(len(encoded_message))]
-            for j in range(rails)]
-    dir_down = None
-    row, col = 0, 0
-    for i in range(len(encoded_message)):
-        if row == 0:
-            dir_down = True
-        if row == rails - 1:
-            dir_down = False
-        rail[row][col] = '*'
-        col += 1
-        if dir_down:
-            row += 1
-        else:
-            row -= 1
-    index = 0
-    for i in range(rails):
-        for j in range(len(encoded_message)):
-            if ((rail[i][j] == '*') and
-                    (index < len(encoded_message))):
-                rail[i][j] = encoded_message[index]
-                index += 1
-    result = []
-    row, col = 0, 0
-    for i in range(len(encoded_message)):
-        if row == 0:
-            dir_down = True
-        if row == rails - 1:
-            dir_down = False
+def encode(message: str, rails: int) -> str:
+    """
+    Encode a given message
 
-        if (rail[row][col] != '*'):
-            result.append(rail[row][col])
-            col += 1
-        if dir_down:
-            row += 1
-        else:
-            row -= 1
-    return "".join(result)
+    :params:
+    message: str
+    rails: int
+
+    :return:
+    encoded_message: str
+    """
+    p = fence_pattern(rails)
+    return ''.join(sorted(message, key=lambda i: next(p)))
+
+
+def decode(encoded_message: str, rails: int) -> str:
+    """
+    Decode a given message
+
+    :params:
+    encoded_message: str
+    rails: int
+
+    :return:
+    decoded_message: str
+    """
+    p = fence_pattern(rails)
+    indexes = sorted(range(len(encoded_message)), key=lambda i: next(p))
+    result = [''] * len(encoded_message)
+    for i, c in zip(indexes, encoded_message):
+        result[i] = c
+    return ''.join(result)
+
+
+def fence_pattern(n: int) -> cycle:
+    """
+    Zigzag pattern
+    :param:
+    n: int
+
+    :return:
+    pattern: itertools.cycle
+    """
+    r = list(range(n))
+    return cycle(r + r[-2:0:-1])
