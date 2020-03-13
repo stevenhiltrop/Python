dcc2b914757cf1b6ecec8ff7817fd2aa8169f28b
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-03-13 17:27:28 +0700

    Added: robot_name.py commentary and type hinting on solution

--- Exercism/python/robot-name/robot_name.py
+++ Exercism/python/robot-name/robot_name.py
@@ -1,14 +1,31 @@
-from random import randint, choice
+from random import choices, seed
 from string import ascii_uppercase
 
 
+def create_name() -> str:
+    """
+    Generate a name with 2 uppercase letters and 3 numbers
+
+    :return:
+    name: str
+    """
+    seed()
+    digits = [str(digit) for digit in range(10)]
+    return ''.join(choices(ascii_uppercase, k=2) + choices(digits, k=3))
+
+
 class Robot:
+    """
+    A cool robot with a name
+    """
     def __init__(self):
         """
-        Define a random name for the new robot
+        Create a name on initialisation
         """
-        self.initials = ''.join(choice(ascii_uppercase) for x in range(2))
-        self.numbers = ''.join(str(randint(0, 9)) for number in range(3))
+        self.name = create_name()
 
-    def name(self):
-        return self.initials + self.numbers
+    def reset(self):
+        """
+        Reset the name of the robot
+        """
+        self.name = create_name()
