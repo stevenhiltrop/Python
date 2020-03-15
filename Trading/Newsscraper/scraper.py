7965b6182344647ae22dfc60149cf90fd34fc933
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-03-15 15:39:56 +0700

    Added: scraper.py financial data retrieval using yfinance

--- Trading/Newsscraper/scraper.py
+++ Trading/Newsscraper/scraper.py
@@ -1,33 +1,25 @@
 # -*- coding: utf-8 -*-
-import ssl
-import urllib.request
+from urllib.request import urlopen
 
-import certifi
+import yfinance as yf
 from bs4 import BeautifulSoup
 
-tickers = list()
-# TODO 1. Get all earningreports
+tickers = str()
+
+# TODO 1. Get all earningreport tickers
+
 source = 'https://earningswhispers.com/calendar'
-with urllib.request.urlopen(source, context=ssl.create_default_context(cafile=certifi.where())) as response:
+with urlopen(source) as response:
     page = response.read()
 
 soup = BeautifulSoup(page, 'html.parser')
 whisper_tickers = soup.find_all('div', attrs={'class': 'ticker'})
 
 for ticker in whisper_tickers:
-    tickers.append(ticker.text)
+    tickers += "{} ".format(ticker.text)
 
-####################################################################################
+# TODO 2. Get financial data through API calls
 
-source = 'https://www.marketwatch.com/tools/earningscalendar'
-with urllib.request.urlopen(source, context=ssl.create_default_context(cafile=certifi.where())) as response:
-    page = response.read()
+tickers = yf.Tickers(tickers)
 
-soup = BeautifulSoup(page, 'html.parser')
-marketwatch_tickers = soup.find_all('td')
-
-for ticker in marketwatch_tickers:
-    if ticker.text.isupper():
-        tickers.append(ticker.text)
-
-# TODO 2. Check if they have enough volume
+# TODO 3. Make conclusions
