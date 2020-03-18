5faaab45d16fa78365f793c49a70db369635458c
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-03-18 20:11:03 +0700

    Changed: scraper.py cloud backup save

--- Trading/Newsscraper/scraper.py
+++ Trading/Newsscraper/scraper.py
@@ -6,6 +6,7 @@ import yfinance
 from bs4 import BeautifulSoup
 from termcolor import colored
 
+source = 'https://earningswhispers.com/calendar'
 tickers = str()
 stocks = list()
 
@@ -23,18 +24,17 @@ def get_stock_information(stock):
     try:
         result["name"] = stock.ticker
         result["price"] = stock.info.get("previousClose")
-        result["float"] = human_format(stock.info.get("floatShares"))
-        result["volume"] = human_format(stock.info.get("volume"))
-        result["marketcap"] = human_format(stock.info.get("marketCap"))
+        result["float"] = stock.info.get("floatShares")
+        result["volume"] = stock.info.get("volume")
+        result["marketcap"] = stock.info.get("marketCap")
         print(colored(f"{stock.ticker} done.", "green"))
     except:
         print(colored(f"{stock.ticker} failed.", "red"))
+
+    # if 5 < result["price"] < 100:
+    #     result["price"] = colored(f"{} done.", "green")
+
     return result
-
-
-# TODO 1. Get all earningreport tickers
-
-source = 'https://earningswhispers.com/calendar'
 
 print(colored("[-] Connecting to earningswhispers.com...", "yellow"))
 
@@ -51,13 +51,7 @@ if page:
         tickers += f"{ticker.text} "
     print(colored(f"[+] Tickers: {tickers.replace(' ', ', ')}", "green"))
 
-    # TODO 2. Get financial data through API calls
     tickers = yfinance.Tickers(tickers)
-    # https://github.com/ranaroussi/yfinance
-
-    # print(colored("[-] Downloading ticker data", "yellow"))
-    # data = yf.download(tickers, period="1d", group_by="ticker")
-    # print(colored("[+] Done!", "green"))
 
     print(colored("[-] Getting information on each ticker (this might take a while)", "yellow"))
     for ticker in tickers.tickers:
@@ -65,7 +59,7 @@ if page:
         stocks.append(ticker)
 
     # TODO 3. Set filter criteria
-    # check_criteria()
+
 
     # TODO 4. Make conclusions
     print(pandas.DataFrame(stocks).to_string(index=False))
