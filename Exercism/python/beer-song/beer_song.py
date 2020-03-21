ef1134e24274cedecc01c55da1d412679ad1c246
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-03-21 14:19:58 +0700

    Added: beer_song.py documentation, verse and beer function, type hinting, solution

--- Exercism/python/beer-song/beer_song.py
+++ Exercism/python/beer-song/beer_song.py
@@ -1,10 +1,48 @@
-def recite(start, take=1):
-    lyrics = list()
-    for bottle in range(start, start - take, -1):
-        remainder = bottle - 1
-        if remainder <= 0:
-            remainder = "no more"
-            start = 99
-        lyrics.append(f"{bottle} bottles of beer on the wall, {bottle} bottles of beer.")
-        lyrics.append(f"Take one down and pass it around, {remainder} bottles of beer on the wall.")
-    return lyrics
+from typing import List
+
+
+def recite(start: int, take=1) -> List[str]:
+    """
+    Recite the lyrics
+
+    :param:
+    start: int
+    take: int [default=1]
+
+    :return:
+    lyrics: List[str]
+    """
+    song = [verse(n) + [""] for n in range(start, start - take, -1)]
+    return [line for v in song for line in v][:-1]
+
+
+def verse(number: int) -> List[str]:
+    """
+    Create a verse from given number
+
+    :param:
+    number: int
+
+    :return:
+    verse: List[str]
+    """
+    return [
+        f"{beer(number).capitalize()} on the wall, {beer(number)}.",
+        (
+            f"Take {'one' if number > 1 else 'it'} down and pass it around"
+            if number else "Go to the store and buy some more"
+        ) + f", {beer(number - 1)} on the wall."
+    ]
+
+
+def beer(number: int) -> str:
+    """
+    Check amount of beers and match words accordingly
+
+    :param:
+    number: int
+
+    :return:
+    lyric: str
+    """
+    return f"{'99' if number == -1 else number if number else 'no more'} bottle{'s' if number != 1 else ''} of beer"
