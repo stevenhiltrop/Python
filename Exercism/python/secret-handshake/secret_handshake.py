cd42ba924892ba510345dcf87c99f6aa7b86ba0f
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-03-23 19:38:11 +0700

    Added: secret_handshake.py functionality

--- Exercism/python/secret-handshake/secret_handshake.py
+++ Exercism/python/secret-handshake/secret_handshake.py
@@ -1,2 +1,16 @@
 def commands(number):
     binary = f'{number:b}'
+    secrets = list()
+    reverse = False
+
+    if len(binary) >= 5 and binary:
+        reverse = True
+
+    binary = binary[-4:]
+
+    for index, char in enumerate(binary):
+        if char
+    1 = "wink"
+    10 = "double blink"
+    100 = "close your eyes"
+    1000 = "jump"
