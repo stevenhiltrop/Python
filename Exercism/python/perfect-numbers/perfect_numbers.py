d208966042418ba33dcb15b77f6db6069bfa43e2
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-03-23 18:38:46 +0700

    Added: perfect_numbers.py functionality

--- Exercism/python/perfect-numbers/perfect_numbers.py
+++ Exercism/python/perfect-numbers/perfect_numbers.py
@@ -1,3 +1,25 @@
-def classify(number):
-    for i in range(1, number + 1):
-        print(divmod(number, i))
+def classify(number: int) -> str:
+    """
+    Classify the given number according to Nicomachus classification.
+
+    :param:
+    number: int
+
+    :return:
+    classification: str
+    """
+    if 0 < number and type(number) is int:
+        aliquots = list()
+        for i in range(1, number):
+            if divmod(number, i)[1] == 0:
+                aliquots.append(i)
+
+        if sum(aliquots) == number:
+            return "perfect"
+        elif sum(aliquots) > number:
+            return "abundant"
+        elif sum(aliquots) < number:
+            return "deficient"
+
+    else:
+        return ValueError("Not a natural number.")
