399cba22b72c15238d78b63fb024957272d0d2b1
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-03-24 11:39:07 +0700

    Added: palindrome_products.py

---
+++ Exercism/python/palindrome-products/palindrome_products.py
@@ -0,0 +1,26 @@
+def largest(max_factor, min_factor=0):
+    """
+    Largest palindrome
+
+    :params:
+    max_factor: int
+    min_factor: int [default=0]
+
+    :return:
+    largest_palindrome: tuple
+    """
+    pass
+
+
+def smallest(max_factor, min_factor=0):
+    """
+    Largest palindrome
+
+    :params:
+    max_factor: int
+    min_factor: int [default=0]
+
+    :return:
+    largest_palindrome: tuple
+    """
+    pass
