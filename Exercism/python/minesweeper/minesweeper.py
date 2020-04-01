883da1bcf0cf1e4d178b97c74cbce41b193f437d
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-04-01 14:51:58 +0700

    Added: minesweeper.py bomb count idea

--- Exercism/python/minesweeper/minesweeper.py
+++ Exercism/python/minesweeper/minesweeper.py
@@ -4,3 +4,53 @@ import numpy
 def annotate(minefield: list):
     # Function body starts here
     matrix = numpy.array(minefield)
+
+    with numpy.nditer(matrix, op_flags=['readwrite']) as iter:
+        for x in iter:
+            x[...] = 2 * x
+
+    """
+#Adds 1 to all of the squares around a bomb.
+def updateValues(rn, c, b):
+
+    #Row above.
+    if rn-1 > -1:
+        r = b[rn-1]
+        
+        if c-1 > -1:
+            if not r[c-1] == '*':
+                r[c-1] += 1
+
+        if not r[c] == '*':
+            r[c] += 1
+
+        if 9 > c+1:
+            if not r[c+1] == '*':
+                r[c+1] += 1
+
+    #Same row.    
+    r = b[rn]
+
+    if c-1 > -1:
+        if not r[c-1] == '*':
+            r[c-1] += 1
+
+    if 9 > c+1:
+        if not r[c+1] == '*':
+            r[c+1] += 1
+
+    #Row below.
+    if 9 > rn+1:
+        r = b[rn+1]
+
+        if c-1 > -1:
+            if not r[c-1] == '*':
+                r[c-1] += 1
+
+        if not r[c] == '*':
+            r[c] += 1
+
+        if 9 > c+1:
+            if not r[c+1] == '*':
+                r[c+1] += 1
+    """
