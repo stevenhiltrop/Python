f17e8c3b29b7c714f78a9dc9cb6f010f30413170
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-04-21 13:50:45 +0700

    Changed: queen_attack.py diagonally method to be more efficient, type hinting, documentation

--- Exercism/python/queen-attack/queen_attack.py
+++ Exercism/python/queen-attack/queen_attack.py
@@ -1,4 +1,11 @@
-def validate_input(row, column):
+def validate_input(row: int, column: int):
+    """
+    Validate if given input matches our criteria
+
+    :params:
+    row: int
+    column: int
+    """
     if row < 0 or column < 0:
         raise ValueError("Position cannot be negative.")
     if row >= 8 or column >= 8:
@@ -6,14 +13,30 @@ def validate_input(row, column):
 
 
 class Queen:
-    def __init__(self, row, column):
+    def __init__(self, row: int, column: int):
+        """
+        Queen object that holds x and y position.
+
+        :params:
+        row: int
+        column: int
+        """
         validate_input(row, column)
         self.row = row
         self.column = column
 
-    def can_attack(self, another_queen):
+    def can_attack(self, another_queen) -> bool:
+        """
+        Check if we can attack the other opponent
+
+        :param:
+        another_queen: Queen object
+
+        :return:
+        can_attack: bool
+        """
         if (self.row, self.column) == (another_queen.row, another_queen.column):
-            raise ValueError("Opponents on same position.")
+            raise ValueError("Opponent is on the same position.")
 
         def horizontally(row):
             return self.row == row
@@ -22,21 +45,17 @@ class Queen:
             return self.column == column
 
         def diagonally(row, column):
-            left_column = right_column = column
+            candidates = set()
 
-            for position in range(row, 8):
-                left_column -= 1
-                right_column += 1
-                return False if (row, left_column) == (row, column) or \
-                               (row, right_column) == (row, column) \
-                    else True
+            for i in range(0, 8):
+                candidates.update([
+                    (self.row + i, self.column + i),
+                    (self.row + i, self.column - i),
+                    (self.row - i, self.column + i),
+                    (self.row - i, self.column - i)]
+                )
 
-            for position in range(8, row, -1):
-                left_column -= 1
-                right_column += 1
-                return False if (row, left_column) == (row, column) or \
-                               (row, right_column) == (row, column) \
-                    else True
+            return True if (row, column) in candidates else False
 
         return horizontally(another_queen.row) or \
                vertically(another_queen.column) or \
