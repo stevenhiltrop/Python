19f00ea45160966901d18ea8ad429a60b739feb4
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-04-21 12:33:25 +0700

    Changed: queen_attack.py changed True and False in diagonally() from can_attack method.

--- Exercism/python/queen-attack/queen_attack.py
+++ Exercism/python/queen-attack/queen_attack.py
@@ -1,18 +1,20 @@
-def validate_position(row, column):
+def validate_input(row, column):
     if row < 0 or column < 0:
         raise ValueError("Position cannot be negative.")
-
     if row >= 8 or column >= 8:
         raise ValueError("The board is 0 - 7 squares. Given position is out of bounds.")
 
 
 class Queen:
     def __init__(self, row, column):
-        validate_position(row, column)
+        validate_input(row, column)
         self.row = row
         self.column = column
 
     def can_attack(self, another_queen):
+        if (self.row, self.column) == (another_queen.row, another_queen.column):
+            raise ValueError("Opponents on same position.")
+
         def horizontally(row):
             return self.row == row
 
@@ -25,15 +27,16 @@ class Queen:
             for position in range(row, 8):
                 left_column -= 1
                 right_column += 1
-                if (row, left_column) == (row, column) or (row, right_column) == (row, column):
-                    return True
+                return False if (row, left_column) == (row, column) or \
+                               (row, right_column) == (row, column) \
+                    else True
 
             for position in range(8, row, -1):
                 left_column -= 1
                 right_column += 1
-                if (row, left_column) == (row, column) or (row, right_column) == (row, column):
-                    return True
-
+                return False if (row, left_column) == (row, column) or \
+                               (row, right_column) == (row, column) \
+                    else True
 
         return horizontally(another_queen.row) or \
                vertically(another_queen.column) or \
