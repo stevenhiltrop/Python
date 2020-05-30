17fec1ecc539e054ffd0bc2fe87cdd2e9604e161
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-05-30 15:51:55 +0700

    Changed: phone_number.py pretty attribute

--- Exercism/python/phone-number/phone_number.py
+++ Exercism/python/phone-number/phone_number.py
@@ -2,14 +2,12 @@ def filter_numbers(number: str) -> str:
     return ''.join(filter(str.isdigit, number))
 
 
-def validate(number: str) -> str:
+def validate(number: str) -> bool:
     n_digit = number[:1]
     x_digit = number[1:]
 
-    if 2 > int(n_digit) or not n_digit.isdigit():
-        raise ValueError(f"{n_digit}: {type(n_digit)}")
-    elif not x_digit.isdigit():
-        raise ValueError(f"{x_digit}")
+    if 2 > int(n_digit) or not n_digit.isdigit() or not x_digit.isdigit():
+        raise ValueError
     else:
         return number
 
@@ -19,8 +17,6 @@ class PhoneNumber:
         digits = filter_numbers(number)
         country_code = digits[:1] if digits.startswith('1') else None
 
-        # TODO: number validation
-
         self.number = digits[1:] if country_code else digits
         self.area_code = validate(self.number[:3])
-        self.pretty = f"({self.area_code}) {validate(number[3:6])}-{number[6:]}"
+        self.pretty = f"({self.area_code}) {number[3:6]}-{number[6:]}"
