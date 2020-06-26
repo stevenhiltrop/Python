5aec75b5b3df5e364980671c604d0bdb5fadb984
stevenhiltrop <stevenhiltrop@gmail.com>
2020-06-26 15:17:11 +0700

    Changed phone_number.py solution

--- Exercism/python/phone-number/phone_number.py
+++ Exercism/python/phone-number/phone_number.py
@@ -1,21 +1,27 @@
-def validate(number: str) -> bool:
-    n_digit = number[:1]
-    x_digit = number[1:]
-
-    if 2 > int(n_digit) or not n_digit.isdigit() or not x_digit.isdigit():
-        raise ValueError
+import re
 
 
-class PhoneNumber:
-    # TODO: validation
-    def __init__(self, number):
-        self.digits = ''.join(filter(str.isdigit, number))
-        self.country_code = self.digits[:1] if self.digits.startswith('1') else None
-        self.area_code = self.number[:3]
+class PhoneNumber(object):
+    _pattern = re.compile(r'''
+        ^\+?1?              # optional literal `+` and country code
+        (?:[-. ]+)?         # optional separators
+        \(?([2-9]\d{2})\)?  # the area code, with or without surrounding parens
+        (?:[-. ]+)?         # optional separators
+        ([2-9]\d{2})        # the exchange code
+        (?:[-. ]+)?         # optional separators
+        (\d{4})$            # the subscriber number
+    ''', re.VERBOSE)
+
+    def __init__(self, phone_number):
+        try:
+            self.area_code, self.exchange, self.subscriber = \
+                    self._pattern.search(phone_number.strip()).groups()
+        except AttributeError:
+            raise ValueError('Invalid `phone_number`')
 
     @property
     def number(self):
-        return self.digits[1:] if self.country_code else self.digits
+        return self.area_code + self.exchange + self.subscriber
 
     def pretty(self):
-        return f"({self.area_code}) {self.number[3:6]}-{self.number[6:]}"
+        return '({}) {}-{}'.format(self.area_code, self.exchange, self.subscriber)
