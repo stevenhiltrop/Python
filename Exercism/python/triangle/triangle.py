40139f7d9ff5fff5ee4b1ea92bf7bc4f40293643
stevenhiltrop <stevenhiltrop@gmail.com>
2020-07-07 16:52:42 +0700

    Added isosceles solution

--- Exercism/python/triangle/triangle.py
+++ Exercism/python/triangle/triangle.py
@@ -1,9 +1,27 @@
 def equilateral(sides):
+    """
+    An equilateral triangle has all three sides the same length
+
+    :param
+    sides: list
+
+    :return
+    is_equilateral: bool
+    """
     return len(set(sides)) == 1 and 0 not in set(sides)
 
 
 def isosceles(sides):
-    pass
+    """
+    An isosceles triangle has at least two sides the same length
+
+    :param
+    sides: list
+
+    :return
+    is_isosceles: bool
+    """
+    return len(set(sides)) <= 2 and 1 not in set(sides)
 
 
 def scalene(sides):
