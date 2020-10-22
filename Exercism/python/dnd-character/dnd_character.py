d4a564c328fe590351f856d5826e3c2812105e16
stevenhiltrop <stevenhiltrop@gmail.com>
2020-10-22 19:24:41 +0700

    Added dnd_character.py functionality

--- Exercism/python/dnd-character/dnd_character.py
+++ Exercism/python/dnd-character/dnd_character.py
@@ -1,3 +1,33 @@
+from random import randint, choice
+
+ABILITIES = [
+    "charisma",
+    "wisdom",
+    "intelligence",
+    "constitution",
+    "dexterity",
+    "strength"
+]
+
+
+def modifier(mod):
+    return round((mod - 10) / 2)
+
+
+def roll_dice():
+    rolls = [randint(1, 6) for roll in range(4)]
+    return sum(sorted(rolls)[-3:])
+
+
 class Character:
     def __init__(self):
-        pass
+        self.charisma = roll_dice()
+        self.wisdom = roll_dice()
+        self.intelligence = roll_dice()
+        self.constitution = roll_dice()
+        self.dexterity = roll_dice()
+        self.strength = roll_dice()
+        self.hitpoints = 10 + modifier(self.constitution)
+
+    def ability(self):
+        return self.__getattribute__(choice(ABILITIES))
