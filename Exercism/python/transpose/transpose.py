ba293f4cfb95d21e3720d1d1415953be44c752d9
stevenhiltrop <stevenhiltrop@gmail.com>
2020-10-22 18:31:06 +0700

    Added transpose.py documentation

--- Exercism/python/transpose/transpose.py
+++ Exercism/python/transpose/transpose.py
@@ -1,6 +1,19 @@
-import itertools
+from itertools import zip_longest
 
 
-def transpose(s) -> str:
-    a = itertools.zip_longest(*s.splitlines(), fillvalue='$')
-    return '\n'.join(''.join(w).rstrip('$').replace('$', ' ') for w in a)
+def transpose(lines: str) -> str:
+    """
+    Transpose text input to a string matrix
+
+    :param:
+    lines: str
+
+    :return:
+    transposed_string: str
+        A string is returned with new lines to make it look like a matrix.
+    """
+    # fill up empty space with an uncommon value '$'
+    iterable = zip_longest(*lines.splitlines(), fillvalue='$')
+
+    # replace '$' with spaces
+    return '\n'.join(''.join(word).rstrip('$').replace('$', ' ') for word in iterable)
