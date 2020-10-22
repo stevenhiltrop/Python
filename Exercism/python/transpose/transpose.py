8dcf7d75a9b86e6403031843baca35d74edd17e5
stevenhiltrop <stevenhiltrop@gmail.com>
2020-10-22 18:11:15 +0700

    Changed transpose.py input parsing

--- Exercism/python/transpose/transpose.py
+++ Exercism/python/transpose/transpose.py
@@ -1,5 +1,6 @@
 def transpose(lines):
     if lines:
-        return [''.join(tup) for tup in (zip(*lines))]
+        result = [''.join(tup) for tup in (zip(*lines.splitlines()))]
+        return '\n'.join(result)
     else:
         return lines
