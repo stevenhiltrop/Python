cb8330d9b6b971f6f0d297080c82e7dcfad7df83
stevenhiltrop <stevenhiltrop@gmail.com>
2021-01-04 19:24:59 +0700

    Moved Exercism and Trading sub repositories to their own.

--- Exercism/python/acronym/acronym.py
+++
@@ -1,6 +0,0 @@
-from re import findall
-
-
-def abbreviate(words):
-    words = words.replace("'", "").title()
-    return ''.join(findall('[A-Z]', words))
--- Exercism/python/isogram/isogram.py
+++
@@ -1,8 +0,0 @@
-def is_isogram(string):
-    string = string.lower()
-
-    for char in string:
-        if char not in ['-', ' '] and string.count(char) > 1:
-            return False
-
-    return True
